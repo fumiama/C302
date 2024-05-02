728f72e8f68b05159de232b0bc7c8f4edf18b9ee
源文雨 <41315874+fumiama@users.noreply.github.com>
2024-05-02 23:50:26 +0900

    feat: update urls

--- c302.h
+++ c302.h
@@ -26,10 +26,10 @@ static const char* urls[] = {
     "https://bocchi.rocks/assets/img/page/character/nijika/main.png",   // 伊地知虹夏
     "https://bocchi.rocks/assets/img/page/character/ryo/main.png",      // 山田リョウ
     "https://bocchi.rocks/assets/img/page/character/ikuyo/main.png",    // 喜多郁代
-    "https://orthrossoft.jp/assets/images/character/whole_body-yui.png",    // 片桐ユイ
-    "https://orthrossoft.jp/assets/images/character/whole_body-yaeka.png"   // 片桐ヤエカ
+    "https://gitea.seku.su/fumiama/C302/raw/branch/main/imgs/yui.png",    // 片桐ユイ
+    "https://gitea.seku.su/fumiama/C302/raw/branch/main/imgs/yaeka.png"   // 片桐ヤエカ
 };
 
-static const uint16_t urlsl[] = {62, 62, 59, 61, 65, 67};
+static const uint16_t urlsl[] = {62, 62, 59, 61, 63, 65};
 
 #endif
